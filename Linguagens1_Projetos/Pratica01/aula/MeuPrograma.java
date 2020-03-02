f994ca12ad2a44b5d68620f6de8ee9f9d80574a4
Murilo-ZC <carvalho.zanini@gmail.com>
2020-03-02 11:25:56 -0300

    Meu arquivo java do primeiro programa

---
+++ Linguagens1_Projetos/Pratica01/aula/MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma{
+    public static void main(String[] args) {
+        System.out.println("Minha Primeira Aplicação Java!");
+        System.out.println("Ola Mundo");
+        System.out.println("Estou programando em modo Conan!");
+    }
+}
